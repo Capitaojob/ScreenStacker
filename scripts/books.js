4b22478a4afd186496654664e06178d208e301e6
Joao Bataglia - DVP23 <dvp23@OUROWEB.COM.BR>
2024-01-19 09:47:34 -0300

    fixed proportions on mobile, improved functions and added better removing of books

--- scripts/books.js
+++ scripts/books.js
@@ -6,187 +6,188 @@ let BOOKS = getBooks();
 const bookshelfDiv = document.querySelector("#bookshelf");
 
 export function addBook(book = null, name = null, mediaType = null, length = null, completed = null) {
-  const bookshelfShelfs = getBookshelfShelfs();
-  const shelf = findOrCreateShelf(bookshelfShelfs);
+	const bookshelfShelfs = getBookshelfShelfs();
+	const shelf = findOrCreateShelf(bookshelfShelfs);
 
-  const newBook = createBookElement(book, name, mediaType, length, completed);
-  shelf.appendChild(newBook);
+	const newBook = createBookElement(book, name, mediaType, length, completed);
+	shelf.appendChild(newBook);
 }
 
 export function updateBook(id, name = null, mediaType = null, length = null, completed = null) {
-  if (id === null || id === "") return;
+	if (id === null || id === "") return;
 
-  BOOKS = BOOKS.map((book) => {
-    if (book.id !== id) return book;
-    else {
-      if (completed === "true") completed = true;
-      else if (completed === "false") completed = false;
+	BOOKS = BOOKS.map((book) => {
+		if (book.id !== id) return book;
+		else {
+			if (completed === "true") completed = true;
+			else if (completed === "false") completed = false;
 
-      const updatedBook = {
-        id: book.id,
-        name,
-        mediaType,
-        length,
-        completed,
-        height: book.height,
-        width: book.width,
-        color: book.color,
-        decoration: book.decoration,
-        label: book.label,
-        fancy: book.fancy,
-      };
-      return updatedBook;
-    }
-  });
+			const updatedBook = {
+				id: book.id,
+				name,
+				mediaType,
+				length,
+				completed,
+				height: book.height,
+				width: book.width,
+				color: book.color,
+				decoration: book.decoration,
+				label: book.label,
+				fancy: book.fancy,
+			};
+			return updatedBook;
+		}
+	});
 
-  saveBooks();
-  renderBooks();
+	saveBooks();
+	renderBooks();
 }
 
 function getBooks() {
-  return JSON.parse(localStorage.getItem("BOOKS")) ?? [];
+	return JSON.parse(localStorage.getItem("BOOKS")) ?? [];
 }
 
 function getBookRandomValues() {
-  const height = Math.round(Math.random() * 30) + 70 + "%";
-  const width = Math.round(Math.random() * 20) + 80 + "px"; //80px - 100px
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += Math.floor(Math.random() * 16).toString(16);
-  }
-  const label = Math.round(Math.random()) === 1 ? true : false;
+	const height = Math.round(Math.random() * 30) + 70 + "%";
+	const width = Math.round(Math.random() * 20) + 80 + "px"; //80px - 100px
+	let color = "#";
+	for (let i = 0; i < 6; i++) {
+		color += Math.floor(Math.random() * 16).toString(16);
+	}
+	const label = Math.round(Math.random()) === 1 ? true : false;
 
-  return {
-    id: generateRandomId(),
-    height,
-    width,
-    color,
-    decoration: Math.round(Math.random()) === 1 ? true : false,
-    label,
-    fancy: label ? (Math.round(Math.random()) === 1 ? true : false) : false,
-  };
+	return {
+		id: generateRandomId(),
+		height,
+		width,
+		color,
+		decoration: Math.round(Math.random()) === 1 ? true : false,
+		label,
+		fancy: label ? (Math.round(Math.random()) === 1 ? true : false) : false,
+	};
 }
 
 function getBookshelfShelfs() {
-  return bookshelfDiv.querySelectorAll(".shelf");
+	return bookshelfDiv.querySelectorAll(".shelf");
 }
 
 function findOrCreateShelf(bookshelfShelfs) {
-  const lastShelf = bookshelfShelfs[bookshelfShelfs.length - 1];
-  const lastShelfChilds = lastShelf?.childNodes.length || 0;
+	const lastShelf = bookshelfShelfs[bookshelfShelfs.length - 1];
+	const lastShelfChilds = lastShelf?.childNodes.length || 0;
 
-  if (bookshelfShelfs.length === 0 || lastShelf === null || lastShelfChilds >= MAX_BOOKS) {
-    const newShelf = createShelfElement();
-    bookshelfDiv.appendChild(newShelf);
-    return newShelf;
-  }
+	if (bookshelfShelfs.length === 0 || lastShelf === null || lastShelfChilds >= MAX_BOOKS) {
+		const newShelf = createShelfElement();
+		bookshelfDiv.appendChild(newShelf);
+		return newShelf;
+	}
 
-  return lastShelf;
+	return lastShelf;
 }
 
 function createShelfElement() {
-  const shelf = document.createElement("div");
-  shelf.classList.add("shelf");
-  return shelf;
+	const shelf = document.createElement("div");
+	shelf.classList.add("shelf");
+	return shelf;
 }
 
 function createBookElement(book, name, mediaType, length, completed) {
-  const newBook = document.createElement("div");
-  newBook.classList.add("book");
-  newBook.tabIndex = 100;
+	const newBook = document.createElement("div");
+	newBook.classList.add("book");
+	newBook.tabIndex = 100;
 
-  if (book === null) {
-    book = getBookRandomValues();
-    book = { name, mediaType, length, completed, ...book };
-    saveBook(book);
-  }
+	if (book === null) {
+		book = getBookRandomValues();
+		book = { name, mediaType, length, completed, ...book };
+		saveBook(book);
+	}
 
-  applyBookStyles(newBook, book);
+	applyBookStyles(newBook, book);
 
-  addBookClickEvent(newBook);
+	addBookClickEvent(newBook);
 
-  return newBook;
+	return newBook;
 }
 
 function applyBookStyles(newBook, book) {
-  newBook.style.height = book.height;
-  newBook.style.width = book.width;
-  newBook.style.backgroundColor = book.color;
-  if (book.decoration) newBook.classList.add("decorated");
-  if (book.label) {
-    newBook.classList.add("labeled");
-    createAndAppendLabel(newBook, book.name, book.fancy);
-  } else {
-    createAndAppendTitle(book.name, newBook, "cover");
-  }
-  newBook.setAttribute("id", book.id);
+	newBook.style.height = book.height;
+	newBook.style.width = book.width;
+	newBook.style.backgroundColor = book.color;
+	if (book.decoration) newBook.classList.add("decorated");
+	if (book.label) {
+		newBook.classList.add("labeled");
+		createAndAppendLabel(newBook, book.name, book.fancy);
+	} else {
+		createAndAppendTitle(book.name, newBook, "cover");
+	}
+	newBook.setAttribute("id", book.id);
 }
 
 function addBookClickEvent(newBook) {
-  newBook.addEventListener("click", (e) => {
-    const id = e.target.id;
-    const bookData = BOOKS.find((book) => book.id == id);
-    openBookForm(
-      bookData.name,
-      bookData.mediaType,
-      bookData.length,
-      bookData.completed,
-      bookData.name,
-      "Save Changes",
-      "./images/pagebackground.jpg",
-      "initial",
-      bookData.id
-    );
-  });
+	newBook.addEventListener("click", (e) => {
+		const id = e.target.id;
+		const bookData = BOOKS.find((book) => book.id == id);
+		openBookForm(bookData.name, bookData.mediaType, bookData.length, bookData.completed, bookData.name, "Save Changes", "./images/pagebackground.jpg", "initial", bookData.id);
+	});
 }
 
 function createAndAppendLabel(newBook, bookName, isFancy) {
-  const label = document.createElement("div");
-  label.classList.add("label");
-  if (isFancy) label.classList.add("fancy");
-  createAndAppendTitle(bookName, label, "label");
-  newBook.appendChild(label);
+	const label = document.createElement("div");
+	label.classList.add("label");
+	if (isFancy) label.classList.add("fancy");
+	createAndAppendTitle(bookName, label, "label");
+	newBook.appendChild(label);
 }
 
 function createAndAppendTitle(title, parent, type) {
-  const label = document.createElement("p");
+	const label = document.createElement("p");
 
-  if (type == "label") {
-    label.classList.add("vertical__text");
-    label.innerText = title;
-  } else {
-    label.innerText = title
-      .split(" ")
-      .map((word) => word.charAt(0))
-      .join("");
-  }
+	if (type == "label") {
+		label.classList.add("vertical__text");
+		label.innerText = title;
+	} else {
+		label.innerText = title;
+		// .split(" ")
+		// .map((word) => word.charAt(0))
+		// .join("");
+	}
 
-  parent.appendChild(label);
+	parent.appendChild(label);
 }
 
 function saveBook(book) {
-  BOOKS.push(book);
-  saveBooks();
+	BOOKS.push(book);
+	saveBooks();
 }
 
 function saveBooks() {
-  localStorage.setItem("BOOKS", JSON.stringify(BOOKS));
+	localStorage.setItem("BOOKS", JSON.stringify(BOOKS));
 }
 
 export function deleteBook(id) {
-  BOOKS = BOOKS.filter((book) => book.id !== id);
-  saveBooks();
-  renderBooks();
+	BOOKS = BOOKS.filter((book) => book.id !== id);
+	saveBooks();
+	renderBooks();
 }
 
-function renderBooks() {
-  bookshelfDiv.innerHTML = "";
+export function renderBooks() {
+	bookshelfDiv.innerHTML = "";
 
-  BOOKS.forEach((book) => {
-    addBook(book);
-  });
+	if (BOOKS.length === 0) {
+		const shelfElement = document.createElement("div");
+		shelfElement.classList.add("shelf");
+
+		bookshelfDiv.appendChild(shelfElement);
+	} else {
+		BOOKS.forEach((book) => {
+			addBook(book);
+		});
+	}
+}
+
+export function clearBooks() {
+	BOOKS = [];
 }
 
 BOOKS.forEach((book) => {
-  addBook(book);
+	addBook(book);
 });
--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-import { addBook, deleteBook, updateBook } from "./books.js";
+import { addBook, clearBooks, deleteBook, renderBooks, updateBook } from "./books.js";
 
 const createBookButton = document.querySelector("[data-create-book]");
 const clearBookButton = document.querySelector("[data-clear-book]");
@@ -11,93 +11,89 @@ const idElement = bookForm.querySelector("#book__id");
 const bookFormInputs = bookForm.querySelectorAll(".form__input");
 
 createBookButton.addEventListener("click", (e) => {
-  openBookForm();
+	openBookForm();
 });
 
 addBookButton.addEventListener("click", (e) => {
-  let valid = true;
+	let valid = true;
 
-  bookFormInputs.forEach((input) => {
-    if (!input.checkValidity()) {
-      valid = false;
-      return;
-    }
-  });
+	bookFormInputs.forEach((input) => {
+		if (!input.checkValidity()) {
+			valid = false;
+			return;
+		}
+	});
 
-  if (valid === false) return;
+	if (valid === false) return;
 
-  e.preventDefault();
+	e.preventDefault();
 
-  if (e.target.innerText === "Create Book") {
-    addBook(null, bookFormInputs[0].value ?? "", bookFormInputs[1].value ?? "", bookFormInputs[2].value ?? 1, bookFormInputs[3].checked ?? false);
-  } else {
-    updateBook(
-      idElement.innerHTML,
-      bookFormInputs[0].value ?? "",
-      bookFormInputs[1].value ?? "",
-      bookFormInputs[2].value ?? 1,
-      bookFormInputs[3].checked ?? false
-    );
-  }
+	if (e.target.innerText === "Create Book") {
+		addBook(null, bookFormInputs[0].value ?? "", bookFormInputs[1].value ?? "", bookFormInputs[2].value ?? 1, bookFormInputs[3].checked ?? false);
+	} else {
+		updateBook(idElement.innerHTML, bookFormInputs[0].value ?? "", bookFormInputs[1].value ?? "", bookFormInputs[2].value ?? 1, bookFormInputs[3].checked ?? false);
+	}
 
-  closeBookForm();
+	closeBookForm();
 });
 
 clearBookButton.addEventListener("click", (e) => {
-  localStorage.removeItem("BOOKS");
+	localStorage.removeItem("BOOKS");
+	clearBooks();
+	renderBooks();
 });
 
 closeFormButton.addEventListener("click", () => {
-  closeBookForm();
+	closeBookForm();
 });
 
 deleteBookButton.addEventListener("click", (e) => {
-  deleteBook(idElement.innerHTML); //e.target.parentElement.querySelector("#book__id")
-  closeBookForm();
+	deleteBook(idElement.innerHTML); //e.target.parentElement.querySelector("#book__id")
+	closeBookForm();
 });
 
 bookForm.querySelector("select").addEventListener("change", (e) => {
-  const type = e.target.value;
-  if (type === "book") {
-  } else if (type === "movie") {
-  } else if (type === "series") {
-  }
+	const type = e.target.value;
+	if (type === "book") {
+	} else if (type === "movie") {
+	} else if (type === "series") {
+	}
 });
 
 export function openBookForm(
-  name = "",
-  mediaType = "book",
-  length = 1,
-  completed = false,
-  title = "Create Book",
-  buttonText = "Create Book",
-  imgSrc = "./images/formside.png",
-  deleteBtnDisplay = "none",
-  id = ""
+	name = "",
+	mediaType = "book",
+	length = 1,
+	completed = false,
+	title = "Create Book",
+	buttonText = "Create Book",
+	imgSrc = "./images/formside.png",
+	deleteBtnDisplay = "none",
+	id = ""
 ) {
-  if (completed === "true") completed = true;
-  else if (completed === "false") completed = false;
+	if (completed === "true") completed = true;
+	else if (completed === "false") completed = false;
 
-  bookFormInputs[0].value = name;
-  bookFormInputs[1].value = mediaType;
-  bookFormInputs[2].value = length;
-  bookFormInputs[3].checked = completed;
-  bookForm.querySelector("h3").innerHTML = title;
-  bookForm.querySelector("button").innerHTML = buttonText;
-  bookForm.querySelector("img").src = imgSrc;
-  bookForm.querySelector(".btn__delete__item").style.display = deleteBtnDisplay;
+	bookFormInputs[0].value = name;
+	bookFormInputs[1].value = mediaType;
+	bookFormInputs[2].value = length;
+	bookFormInputs[3].checked = completed;
+	bookForm.querySelector("h3").innerHTML = title;
+	bookForm.querySelector("button").innerHTML = buttonText;
+	bookForm.querySelector("img").src = imgSrc;
+	bookForm.querySelector(".btn__delete__item").style.display = deleteBtnDisplay;
 
-  const bookIdElement = bookForm.querySelector("#book__id");
-  if (id === "") {
-    bookIdElement.style.display = "none";
-  } else {
-    bookIdElement.style.display = "block";
-    bookIdElement.innerText = id;
-  }
+	const bookIdElement = bookForm.querySelector("#book__id");
+	if (id === "") {
+		bookIdElement.style.display = "none";
+	} else {
+		bookIdElement.style.display = "block";
+		bookIdElement.innerText = id;
+	}
 
-  bookForm.classList.remove("hidden");
+	bookForm.classList.remove("hidden");
 }
 
 function closeBookForm() {
-  bookForm.classList.add("hidden");
+	bookForm.classList.add("hidden");
 }
